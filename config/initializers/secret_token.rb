360828843bdfc6daa57c3d695a63a90894ee1e25
Enddy Dumbrique <enddynayn@hotmail.com>
2013-08-12 20:10:00 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skilltrade::Application.config.secret_key_base = 'e43ff2bbf0c88b0828539d2e77846158b6df35770b46e37d96a0180db4b38f89e8d32152bdd9e6c2248193216e057a46056cfad25a22426ad6f4f0e4e383cfda'
